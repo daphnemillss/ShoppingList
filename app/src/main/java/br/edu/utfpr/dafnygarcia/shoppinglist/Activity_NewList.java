1ec3e79ff5cc157de2b01c80b26095a1461d0e19
daphnemillss <dafnymastrelly@gmail.com>
2018-11-19 14:33:34 -0200

    Add melhoras

--- app/src/main/java/br/edu/utfpr/dafnygarcia/shoppinglist/Activity_NewList.java
+++ app/src/main/java/br/edu/utfpr/dafnygarcia/shoppinglist/Activity_NewList.java
@@ -3,6 +3,7 @@ package br.edu.utfpr.dafnygarcia.shoppinglist;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -25,6 +26,9 @@ public class Activity_NewList extends AppCompatActivity {
                 productList();
             }
         });
+
+        ActionBar ab = getSupportActionBar();
+        ab.setDisplayHomeAsUpEnabled(true);
     }
 
     public void productList(){
